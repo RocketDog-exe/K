65f4e87274c0f29a434443aa65b541ca6a3e28a6
RocketDog-exe <danilivanov@vesele-gimnaz.com>
2020-04-30 14:03:23 +0300

    Много чего сделал мой учитель!!!

--- app/src/main/java/com/example/k/MainActivity.java
+++ app/src/main/java/com/example/k/MainActivity.java
@@ -2,16 +2,25 @@ package com.example.k;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     EditText editText1;
     RadioButton radioButton1, radioButton2;
     double edTxtDouble;
@@ -25,13 +34,44 @@ public class MainActivity extends AppCompatActivity {
         radioButton1 = findViewById(R.id.radioButton1);
         radioButton2 = findViewById(R.id.radioButton2);
         editText1 = findViewById(R.id.editText1);
-        edTxtDouble = Double.parseDouble(editText1.getText().toString());
+        textView = findViewById(R.id.textView);
+        MyTask mt = new MyTask();
+        mt.execute();
+
 
     }
 
     public void onClick1(View view) {
+        edTxtDouble = Double.parseDouble(editText1.getText().toString());
         if (radioButton1.isChecked()) Toast.makeText(this, Double.toString(edTxtDouble*dollar), Toast.LENGTH_LONG).show();
         if (radioButton2.isChecked()) Toast.makeText(this, Double.toString(edTxtDouble*euro), Toast.LENGTH_LONG).show();
 
     }
+
+class MyTask extends AsyncTask<Void, Void, Void> {
+    String s = "";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.facebook.com").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (doc!=null)
+            s = doc.title();
+        else
+            s = "Ошибка";
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        textView.setText(s);
+    }
+}
+
 }
